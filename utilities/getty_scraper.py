f07ecac879d15657f8d92eda6e02862cc38d28ab
Daniel Tan <50669678+danieltan2018@users.noreply.github.com>
2026-03-12 18:31:33 +0800

    Scraper updates

--- utilities/getty_scraper.py
+++ utilities/getty_scraper.py
@@ -4,7 +4,7 @@ from unidecode import unidecode
 
 base_url = "https://gettymusic.store"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"
 }
 
 SONG_LINKS = []
--- utilities/sgm_scraper.py
+++ utilities/sgm_scraper.py
@@ -1,5 +1,4 @@
 import re
-import threading
 from concurrent.futures import ThreadPoolExecutor
 
 import requests
@@ -8,7 +7,7 @@ from unidecode import unidecode
 
 base_url = "https://sovereigngracemusic.com/music/songs/"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"
 }
 
 
